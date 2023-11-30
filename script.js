621bb38f28e087ba5db5f548e854d5497f96e740
kumarAjay10 <144784190+kumarAjay10@users.noreply.github.com>
2023-11-30 22:10:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+        function displayResult() {
+            let firstName = document.getElementById('firstName').value;
+            let lastName = document.getElementById('lastName').value;
+            let phoneNumber = document.getElementById('phoneNumber').value;
+            let email = document.getElementById('email').value;
+
+            let result = "First Name: " + firstName + "\n" +
+                               "Last Name: " + lastName + "\n" +
+                               "Phone Number: " + phoneNumber + "\n" +
+                               "Email ID: " + email;
+
+            alert(result);
+        }
